95d96f159daf69304b0e22b4e68f5b8cc4af529c
B00705025 <B00705025@ntu.edu.tw>
2013-10-26 01:03:46 +0800

    Add table.php to file views which is a webpage to show the viewlist and link to personl profile page (in user/profile).

---
+++ application/views/table.php
@@ -0,0 +1,16 @@
+<?php include("_header.php"); ?> 
+    <div class="container">
+    	<table border=1> 
+    		<tr>
+    			<td>Name</td>
+    			<td>StudentID</td>
+    		</tr>
+    		
+    		<?php foreach ($text as $element):?>
+    		<tr>
+    			<td><a href= "<?=site_url("user/profile?=")?>"> <?php echo $element->username;?></a></td>
+    			<td><?php echo $element->studentid;?></td>
+    		</tr>
+    		<?php endforeach;?>
+    	</table>    
+    </div>
