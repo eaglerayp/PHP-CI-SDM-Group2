71fdfb40cf49bbe0ab3168bd725b2f2026d89140
B00705025 <B00705025@ntu.edu.tw>
2013-10-26 01:01:11 +0800

    Add viewlist.php to file controllers which is a php file to control, transmit data and call table.php.

---
+++ application/controllers/viewlist.php
@@ -0,0 +1,45 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');  
+      
+    class Viewlist extends MY_Controller {  
+	
+	/* function for table.php */
+    public function table(){
+	    $this->load->model('listmodel','',TRUE);
+	    $rows = $this->listmodel->getRows();
+	    $rawdata = $this->listmodel->getData();
+	    
+	    /* The for loop is to reorganize the data which will make view php file easier to print. 
+	     * Because one person can have more than a studentid 
+	     * $senddata is the tansmitted object to test.php */
+	    for($i=0; $i<$rows; $i++){
+	    	$senddata[$i]->userid = $rawdata[$i]->userid;		// record userid which is used to send to user/profile
+	    	$senddata[$i]->username = $rawdata[$i]->username;
+	    	$senddata[$i]->studentid = $rawdata[$i]->studentid;
+	    	
+	    	if(($i+1)<$rows){
+	    		/* This is the condition which the person has the second studentid*/
+	    		if($rawdata[$i]->username == $rawdata[$i+1]->username){
+	    			$senddata[$i]->studentid = $senddata[$i]->studentid.",".$rawdata[$i+1]->studentid;
+	    			// append the second studentid to $senddata[$i]->studentid
+	    			
+					if(($i+2)<$rows){
+						/* This is the condition which the person has the third studentid*/
+						if($rawdata[$i+1]->username == $rawdata[$i+2]->username){
+							$senddata[$i]->studentid = $senddata[$i]->studentid.",".$rawdata[$i+2]->studentid;
+							// append the third studentid to $senddata[$i]->studentid
+							
+							$i++; // avoid to loop to the same username
+						}
+					}
+	    			$i++;	// avoid to loop to the same username
+	    		}
+	    	}
+	    }
+	    
+	    $data['text'] = $senddata;	// the organized data
+	    
+	    $this->load->view('table', $data);	// load table.php
+	}
+        
+    }  
+
